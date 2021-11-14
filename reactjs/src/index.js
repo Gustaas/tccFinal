95ff95496889509bda69bc4a8eeaa6d074f6fd30
Igor Lima Charles <igorlc2004@gmail.com>
2021-11-14 19:51:24 -0300

    Correção do Arquivo "Routes"

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Index from './routes'
+import Final from './routes'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Index/>
+    <Final/>
   </React.StrictMode>,
   document.getElementById('root')
   
